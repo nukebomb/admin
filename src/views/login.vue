44a28245445d79b8b28cb92710a573448c4138fc
nukebomb <nukebomb@sina.cn>
2018-10-08 23:09:49 +0800

    add login-page

---
+++ src/views/login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="login-page">
+        here is login login-page
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login'
+}
+</script>
+
+<style>
+
+</style>
