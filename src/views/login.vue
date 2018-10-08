b67fea15ac4b77936a81417ccb4ed6df501e3933
nukebomb <nukebomb@sina.cn>
2018-10-08 23:54:13 +0800

    add login.vue

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,15 +1,51 @@
 <template>
-    <div class="login-page">
-        here is login login-page
-    </div>
+  <div class="login-page">
+    <transition name="form-fade" mode="in-out">
+			<section class="form_contianer">
+				<div class="manage_tip">
+					<p>elm后台管理系统</p>
+				</div>
+				<el-form :model="loginForm" :rules="rules" ref="loginForm">
+					<el-form-item prop="username">
+						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
+					</el-form-item>
+					<el-form-item prop="password">
+						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
+					</el-form-item>
+					<el-form-item>
+						<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
+					</el-form-item>
+				</el-form>
+				<p class="tip">温馨提示：</p>
+				<p class="tip">未登录过的新用户，自动注册</p>
+				<p class="tip">注册过的用户可凭账号密码登录</p>
+			</section>
+	  </transition>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'login'
+  name: "login",
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
+    }
+  },
+  methods: {
+    submitForm() {}
+  }
 }
 </script>
 
 <style>
-
 </style>
