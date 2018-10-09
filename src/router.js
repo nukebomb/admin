17675c9adee36e6cfe6bf3053a6f413865e51fc4
nukebomb <nukebomb@sina.cn>
2018-10-09 12:02:12 +0800

    添加control-pannnel

--- src/router.js
+++ src/router.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Home from './views/Home.vue'
+import Home from './views/Home.vue'
 import Login from './views/Login.vue'
+import Test from './views/test.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/about',
       name: 'about',
@@ -21,9 +22,14 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
-      path: '/',
+      path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test
     }
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home fillcontainer">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="header-container">
+      <el-breadcrumb separator="|">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/">管理</a></el-breadcrumb-item>
+        <el-breadcrumb-item>列表</el-breadcrumb-item>
+        <el-breadcrumb-item>详情</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="control-pannel">
+      <el-menu class="menu-table" default-active="2" background-color="#324057" text-color="#fff" active-text-color="#ffd04b">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+              <span>内容管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">权限管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-document"></i>
+          <span slot="title">日志管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">访问分析</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-setting"></i>
+          <span slot="title">数据备份</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-setting"></i>
+          <span slot="title">用户管理</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
+<style>
+/*
+ * header-top
+ */
+.header-container {
+  width: 100%;
+  height: 60px;
+  background-color: #eff2f7;
+  padding-left: 200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+/*
+ * control-pannel
+ */
+.control-pannel {
+  width: 180px;
+  height: 100%;
+}
+.menu-table {
+  height: 100%;
+}
+</style>
