96c5e5b71227875d5de9abf3a2f75d04cf61c4fc
nukebomb <nukebomb@sina.cn>
2018-10-09 10:06:54 +0800

    login页面的静态

---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header-container">
+    <el-breadcrumb separator="|">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>列表</el-breadcrumb-item>
+      <el-breadcrumb-item>详情</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,26 +1,21 @@
 <template>
-  <div class="login-page">
-    <transition name="form-fade" mode="in-out">
-			<section class="form_contianer">
-				<div class="manage_tip">
-					<p>elm后台管理系统</p>
-				</div>
-				<el-form :model="loginForm" :rules="rules" ref="loginForm">
-					<el-form-item prop="username">
-						<el-input v-model="loginForm.username" placeholder="用户名"><span>dsfsf</span></el-input>
-					</el-form-item>
-					<el-form-item prop="password">
-						<el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
-					</el-form-item>
-					<el-form-item>
-						<el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
-					</el-form-item>
-				</el-form>
-				<p class="tip">温馨提示：</p>
-				<p class="tip">未登录过的新用户，自动注册</p>
-				<p class="tip">注册过的用户可凭账号密码登录</p>
-			</section>
-	  </transition>
+  <div class="login-page fillcontainer">
+    <div class="login-container">
+      <div class="manage-tip">
+        <p>知识管理后台系统</p>
+      </div>
+      <el-form :model="loginForm" status-icon :rules="rules" label-width="100px" class="login-form">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="loginForm.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm(loginForm)" class="submit-btn">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -37,15 +32,45 @@ export default {
         username: [
           { required: true, message: "请输入用户名", trigger: "blur" }
         ],
-        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" }
+        ]
       }
-    }
+    };
   },
   methods: {
-    submitForm() {}
+    submitForm(loginForm) {
+      console.log(loginForm)
+    }
   }
-}
+};
 </script>
 
 <style>
+.login-page {
+  background-color: #324057;
+}
+.login-container {
+  width: 35%;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.manage-tip {
+  position: absolute;
+  width: 100%;
+  top: -100px;
+  right: -30px;
+  height: 80px;
+}
+.manage-tip p {
+  font-size: 34px;
+  color: #fff;
+}
+.login-form {
+}
+.submit-btn {
+  width: 100%;
+}
 </style>
